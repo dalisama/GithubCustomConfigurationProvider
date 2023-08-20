0c149897da47b1dd43af94fa1c343feb678b19d7
dalisama <med.ali.kachbouri@gmail.com>
2023-08-20 16:06:23 +0200

    set up test envir

--- GithubCustomConfigurationProvider/Program.cs
+++ GithubCustomConfigurationProvider/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace GithubCustomConfigurationProvider
+﻿using Microsoft.Extensions.Configuration;
+using GithubCustomConfigurationProviderLib;
+
+namespace GithubCustomConfigurationProvider
 {
+
     internal class Program
     {
         static void Main(string[] args)
+
         {
             Console.WriteLine("Hello, World!");
+            Environment.SetEnvironmentVariable("REPO", "GithubCustomConfigurationProvider");
+            Environment.SetEnvironmentVariable("FILENAME", "jsconfig.json");
+            Environment.SetEnvironmentVariable("TOKEN", "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");//read only token
+
+            IConfiguration Configuration = new ConfigurationBuilder()
+                         .AddGitHubConfiguration("REPO", "FILENAME", "TOKEN")
+                         .Build();
+
+
+
         }
     }
 }
---
+++ GithubCustomConfigurationProviderLib/GitHubConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GithubCustomConfigurationProviderLib
+{
+    public static class GitHubConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddGitHubConfiguration(this IConfigurationBuilder builder, string repo, string filename, string token)
+        {
+            return builder.Add(new GitHubConfigurationSource(repo, filename, token));
+        }
+    }
+
+}
+
